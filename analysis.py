d11920b07a8e9243013bf645bda3a8a03d61cc6f
simonvaradaraj <s.varadaraj03@gmail.com>
2022-07-23 17:57:38 -0500

    Added the Covid Case Spreadsheet for comparison

--- analysis.py
+++ analysis.py
@@ -3,26 +3,25 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-# Data gathered from https://dshs.texas.gov/coronavirus/AdditionalData.aspx - TEXAS DEPARTMENT OF STATE HEALTH SERVICES
+# Data gathered from https://dshs.texas.gov/coronavirus/AdditionalData.aspx 
+# TEXAS DEPARTMENT OF STATE HEALTH SERVICES - Last Updated July 23rd 2022
 
+# vaccsheets is a dictionary that holds a dataframe interpretation of the excel sheets on the vaccine data spreadsheet
+vaccsheets = pd.read_excel("COV19-vaccine-data-by-county.xlsx", sheet_name=None)
+covidsheets = pd.read_excel("texasnewcases.xlsx", sheet_name=None)
 
-# covidsheets is a dictionary that holds a dataframe interpretation of the excel sheets on the vaccine data spreadsheet
-covidsheets = pd.read_excel("COV19-vaccine-data-by-county.xlsx", sheet_name=None)
-
-
-covidsheets['By Vaccination Date'].name = "Vaccinations in Texas Over Time (not cumulative)"
+# FORMATTING THE VACCINATION SHEET
 
 # dropping useless info on the vaccination spreadsheet
-del covidsheets['By Vaccination Date']['People with at least One Booster Dose']
+del vaccsheets['By Vaccination Date']['People with at least One Booster Dose']
 
 # formatting the dates in the excel sheet to pandas datetime values
-covidsheets['By Vaccination Date']['Vaccination Date'] = pd.to_datetime(covidsheets['By Vaccination Date']['Vaccination Date'], format='%Y-%m-%d %H:%M:%S.%f')
-covidsheets['By Vaccination Date']['Doses Administered'] /= 1000
+vaccsheets['By Vaccination Date']['Vaccination Date'] = pd.to_datetime(vaccsheets['By Vaccination Date']['Vaccination Date'], format='%Y-%m-%d %H:%M:%S.%f')
+vaccsheets['By Vaccination Date']['Doses Administered'] /= 1000
 
 # plotting Vaccine doses over time
-covidsheets['By Vaccination Date'].plot(x = "Vaccination Date", y = ["Doses Administered"])
+vaccsheets['By Vaccination Date'].plot(x = "Vaccination Date", y = ["Doses Administered"])
 plt.ylabel("Doses Administered in thousands")
 
 plt.show()
